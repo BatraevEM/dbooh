9c3ffed61c482d64fa6a761337ca592fd62ab29b
Edward Batraev <batraevem@gmail.com>
2017-12-17 17:12:01 +0400

    setup

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="dbooh",
+    version="0.0.0",
+    description="Home finance",
+    author="Edward Batraev",
+    author_email="BatraevEM@gmail.com",
+    packages=find_packages(),
+)
